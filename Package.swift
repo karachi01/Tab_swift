bbc9913fec0e0df0a3863ff7f0c8e0f3289d4a6e
karachi01 <karachilauren05@gmail.com>
2026-02-19 17:08:07 -0500

    Added app icon

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "JKXH644582",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .heart),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.cyan),
             supportedDeviceFamilies: [
                 .pad,
